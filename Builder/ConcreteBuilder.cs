07a936973af0ffe0fd207698ceaca328fa5ee344
Pc <Pc@DESKTOP-PRAMDG2>
2023-06-21 12:28:16 +0330

    fix

--- Builder/ConcreteBuilder.cs
+++ Builder/ConcreteBuilder.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using RefactoringGuru.DesignPatterns.Builder.Conceptual;
-
+﻿
 namespace Builder
 {
     public class ConcreteBuilder : IBuilder
--- Builder/Director.cs
+++ Builder/Director.cs
@@ -12,25 +12,25 @@ namespace Builder
     // optional, since the client can control builders directly.
     public class Director
     {
-        private IBuilder _builder;
+        private  IBuilder  _builder;
 
         public IBuilder Builder
         {
-            set { _builder = value; }
+            set => _builder = value;
         }
 
         // The Director can construct several product variations using the same
         // building steps.
         public void BuildMinimalViableProduct()
         {
-            this._builder.BuildPartA();
+            _builder.BuildPartA();
         }
 
         public void BuildFullFeaturedProduct()
         {
-            this._builder.BuildPartA();
-            this._builder.BuildPartB();
-            this._builder.BuildPartC();
+            _builder.BuildPartA();
+            _builder.BuildPartB();
+            _builder.BuildPartC();
         }
     }
 }
