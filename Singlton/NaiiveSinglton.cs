cac2ab39ec40f8d2c0120c60720809a3fa324f8d
Pc <Pc@DESKTOP-PRAMDG2>
2023-06-26 16:41:55 +0330

    fix

--- Singlton/NaiiveSinglton.cs
+++ Singlton/NaiiveSinglton.cs
@@ -12,27 +12,27 @@ namespace Singlton
 
     // EN : The Singleton should always be a 'sealed' class to prevent class
     // inheritance through external classes and also through nested classes.
-    public sealed class Singleton
+    public sealed class NaiiveSinglton
     {
         // The Singleton's constructor should always be private to prevent
         // direct construction calls with the `new` operator.
-        private Singleton() { }
+        private NaiiveSinglton() { }
 
         // The Singleton's instance is stored in a static field. There there are
         // multiple ways to initialize this field, all of them have various pros
         // and cons. In this example we'll show the simplest of these ways,
         // which, however, doesn't work really well in multithreaded program.
-        private static Singleton _instance;
+        private static NaiiveSinglton? _instance;
 
         // This is the static method that controls the access to the singleton
         // instance. On the first run, it creates a singleton object and places
         // it into the static field. On subsequent runs, it returns the client
         // existing object stored in the static field.
-        public static Singleton GetInstance()
+        public static NaiiveSinglton GetInstance()
         {
             if (_instance == null)
             {
-                _instance = new Singleton();
+                _instance = new NaiiveSinglton();
             }
             return _instance;
         }
--- Singlton/Program.cs
+++ Singlton/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace Singlton
+﻿using Singlton.Singleton;
+
+namespace Singlton
 {
     
 
@@ -7,8 +9,8 @@
         static void Main(string[] args)
         {
             // The client code.
-            Singleton s1 = Singleton.GetInstance();
-            Singleton s2 = Singleton.GetInstance();
+            NaiiveSinglton s1 = NaiiveSinglton.GetInstance();
+            NaiiveSinglton s2 = NaiiveSinglton.GetInstance();
 
             if (s1 == s2)
             {
@@ -18,6 +20,41 @@
             {
                 Console.WriteLine("Singleton failed, variables contain different instances.");
             }
+
+
+            Console.WriteLine("oooooooooooooooooooooo");
+            // The client code.
+
+
+            Console.WriteLine(
+                "{0}\n{1}\n\n{2}\n",
+                "If you see the same value, then singleton was reused (yay!)",
+                "If you see different values, then 2 singletons were created (booo!!)",
+                "RESULT:"
+            );
+
+            Thread process1 = new Thread(() =>
+            {
+                TestSingleton("FOO");
+            });
+            Thread process2 = new Thread(() =>
+            {
+                TestSingleton("BAR");
+            });
+
+            process1.Start();
+            process2.Start();
+
+
+            process1.Join();
+            process2.Join();
+        }
+
+        public static void TestSingleton(string value)
+        {
+            ThreadSafeSinglton singleton = ThreadSafeSinglton.GetInstance(value);
+            Console.WriteLine(singleton.Value);
         }
     }
+    
 }
