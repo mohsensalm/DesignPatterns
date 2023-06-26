3b0c59b6b07d48d993ce77108b27314a9b97933d
Pc <Pc@DESKTOP-PRAMDG2>
2023-06-26 14:53:17 +0330

    fix

---
+++ Singlton/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Singlton
+{
+    
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // The client code.
+            Singleton s1 = Singleton.GetInstance();
+            s1.someBusinessLogic();
+            Singleton s2 = Singleton.GetInstance();
+
+            if (s1 == s2)
+            {
+                Console.WriteLine("Singleton works, both variables contain the same instance.");
+            }
+            else
+            {
+                Console.WriteLine("Singleton failed, variables contain different instances.");
+            }
+        }
+    }
+}
